2734e85f0eb4598d8d3a8c102e7b5cac507f7147
onsi <onsimini@gmail.com>
2021-02-15 21:21:27 -0500

    cleaner script added

--- BandW-image-cleaner/cleaner.py
+++ BandW-image-cleaner/cleaner.py
@@ -1,1 +1,13 @@
-print(f'hello test of smarth commit 6!')
+import cv2
+
+filename = r'org.jpg'
+img = cv2.imread(filename)
+
+for y in range(len(img)):
+    for x in range(len(img[y])):
+        if img[y, x, 1] < 128:
+            img[y, x] = [0, 0, 0]
+        else:
+            img[y, x] = [255, 255, 255]
+
+cv2.imwrite('cleaned.jpg', img)
